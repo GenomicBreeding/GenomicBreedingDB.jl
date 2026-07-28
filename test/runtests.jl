ee898b3589c1cfa2372d12814c7b0bc074e7f816
jeffersonfparil <jeffersonparil@gmail.com>
2026-07-28 10:23:01 +1000

    formatter/test: applied JuliaFormatter + added dbinit + uploads for ci

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,31 +6,45 @@ using Suppressor, ProgressMeter
 using Random, Dates
 
 try
-    try
-        dbinit()
-    catch
-
-        nothing
-    end
-    fname_trial = simulate_trial()
-    fname_environment = simulate_environment(fname_trial)
-    conn = dbconnect()
-    upload_trial_data!(
-        conn,
-        fname = fname_trial,
-        species = "Acacia neglecta",
-        experiment = "some-exp",
-        treatment = "some_trt",
-        entry_type = "family",
-        population_type = "population",
-        relationship_type = "member_of",
-    )
-    upload_environment_data!(conn, fname = fname_environment, experiment = "exp-1", treatment = "trt-42")
-    rm.([fname_trial, fname_environment])
-    close(conn)
+    dbinit()
 catch
     nothing
 end
+simulate_genomes() |> simulate_trials |> simulate_environments
+simulate_phenomes(readdelimited(Trials, fname = "simulated_trials.tsv"))
+simulate_fit(readjld2(Genomes, fname = "simulated_genomes.jld2"), readjld2(Phenomes, fname = "simulated_phenomes.jld2"))
+upload(
+    "simulated_trials.tsv",
+    species = "Zea mays",
+    experiment = "simulated experiment",
+    treatment = "control",
+    entry_type = "family",
+    population_type = "cultivar",
+    relationship_type = "member_of",
+    verbose = true,
+)
+upload("simulated_environments.tsv", experiment = "simulated experiment", treatment = "control", verbose = true)
+upload(abspath("simulated_phenomes.jld2"), name = "simulated phenomes", notes = "simulated", verbose = true)
+upload(
+    abspath("simulated_reference_genome.fa"),
+    name = "simulated reference genome",
+    notes = "simulated",
+    verbose = true,
+)
+upload(
+    abspath("simulated_genomes.vcf"),
+    name = "simulated genomes",
+    notes = "simulated",
+    fname_reference_genome = abspath("simulated_reference_genome.fa"),
+    verbose = true,
+)
+upload(
+    abspath("simulated_genomes.jld2"),
+    name = "simulated genomes",
+    notes = "simulated",
+    fname_reference_genome = abspath("simulated_reference_genome.fa"),
+    verbose = true,
+)
 
 Documenter.doctest(GenomicBreedingDB)
 
