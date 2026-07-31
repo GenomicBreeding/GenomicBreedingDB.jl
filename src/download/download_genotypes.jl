6dc72bf0be94146df0814789787d559401a5be82
jeffersonfparil <jeffersonparil@gmail.com>
2026-07-31 16:27:02 +1000

    oopsie forgot to add download_genotypes.jl

---
+++ src/download/download_genotypes.jl
@@ -0,0 +1,11 @@
+function download_reference_genomes_paths()::DataFrame
+    DataFrame()
+end
+
+function download_vcfs_paths()::DataFrame
+    DataFrame()
+end
+
+function download_genomes_paths()::DataFrame
+    DataFrame()
+end
